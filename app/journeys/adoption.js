f3c2300bf68a4dc40c7533931bfeb8774c26339c
Priyanka Verma <priyanka.verma@hmcts.net>
2021-12-15 09:40:05 +0000

    PCQ-1162 Adoption Service Setup

---
+++ app/journeys/adoption.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const stepList = {
+    StartPage: 'ApplicantDateOfBirth',
+    ApplicantDateOfBirth: 'ApplicantLanguage',
+    ApplicantLanguage: {
+        otherLanguage: 'ApplicantEnglishLevel',
+        otherwise: 'ApplicantSex'
+    },
+    ApplicantEnglishLevel: 'ApplicantSex',
+    ApplicantSex: 'ApplicantGenderSameAsSex',
+    ApplicantGenderSameAsSex: 'ApplicantSexualOrientation',
+    ApplicantSexualOrientation: 'ApplicantMaritalStatus',
+    ApplicantMaritalStatus: 'ApplicantEthnicGroup',
+    ApplicantEthnicGroup: {
+        White: 'ApplicantEthnicBackgroundWhite',
+        Mixed: 'ApplicantEthnicBackgroundMixed',
+        Asian: 'ApplicantEthnicBackgroundAsian',
+        Black: 'ApplicantEthnicBackgroundBlack',
+        Other: 'ApplicantEthnicBackgroundOther',
+        otherwise: 'ApplicantReligion'
+    },
+    ApplicantEthnicBackgroundWhite: 'ApplicantReligion',
+    ApplicantEthnicBackgroundMixed: 'ApplicantReligion',
+    ApplicantEthnicBackgroundAsian: 'ApplicantReligion',
+    ApplicantEthnicBackgroundBlack: 'ApplicantReligion',
+    ApplicantEthnicBackgroundOther: 'ApplicantReligion',
+    ApplicantReligion: 'ApplicantDisability',
+    ApplicantDisability: {
+        Yes: 'ApplicantDisabilityImplications',
+        otherwise: 'ApplicantPregnant'
+    },
+    ApplicantDisabilityImplications: {
+        Yes: 'ApplicantDisabilityImplicationAreas',
+        otherwise: 'ApplicantPregnant'
+    },
+    ApplicantDisabilityImplicationAreas: 'ApplicantPregnant',
+    ApplicantPregnant: 'EndPage'
+};
+
+module.exports = () => {
+    return {stepList};
+};
