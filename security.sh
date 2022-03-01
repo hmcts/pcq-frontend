23d5176707cf1a4738add4b8f5119a2e63d13646
Priyanka Verma <priyanka.verma@hmcts.net>
2022-03-01 17:43:14 +0000

    commenting

--- security.sh
+++ security.sh
@@ -7,28 +7,23 @@ i=0
   #i=$(((i + 1) % 4))
   #sleep .1
 #done
-if !(curl -s http://0.0.0.0:1001) >/dev/null
-then
-  i=$(((i + 1) % 4))
-  sleep .1
-fi
 
-#echo "ZAP has successfully started"
-#zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
-#zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
+echo "ZAP has successfully started"
+zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
+zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
 #zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
-#zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
-#zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
-#zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescanReport.xml -f xml
+zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescanReport.xml -f xml
 #echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-#chown -R $(id -u):$(id -u) activescan.html
-#chown -R $(id -u):$(id -u) activescanReport.xml
+chown -R $(id -u):$(id -u) activescan.html
+chown -R $(id -u):$(id -u) activescanReport.xml
 
-#cp *.html functional-output/
-#cp activescanReport.xml functional-output/
+cp *.html functional-output/
+cp activescanReport.xml functional-output/
 
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
-#curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp *.* functional-output/
 
 echo
