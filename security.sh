4af4a7095645a383894a1315be5fba54424d91f6
Priyanka Verma <priyanka.verma@hmcts.net>
2022-03-02 15:52:02 +0000

    Testing

--- security.sh
+++ security.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 #echo "${SECURITYCONTEXT}" > /zap/security.context
-zap-x.sh -d -host 0.0.0.0 -port 1001 -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*ruxitagentjs.*)+$' -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*cookies?.*)+$'-config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
+zap-x.sh -d -host 0.0.0.0 -port 1001 -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*ruxitagentjs.*)+$' -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*cookies?.*)+$' -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
 i=0
 #while !(curl -s http://0.0.0.0:1001) >/dev/null; do
   #i=$(((i + 1) % 4))
@@ -12,7 +12,7 @@ export LANG=C.UTF-8
 echo "ZAP has successfully started"
 zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
 zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
-#zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
+zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
 zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescanReport.xml -f xml
