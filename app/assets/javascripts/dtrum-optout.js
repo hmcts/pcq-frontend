fc6e9460434242ac66d7a9b8863da1386635f1d5
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2022-05-26 12:11:50 +0100

    PCQ-971 Set cookie status to determine cookie pref

--- app/assets/javascripts/dtrum-optout.js
+++ app/assets/javascripts/dtrum-optout.js
@@ -4,6 +4,21 @@
     const optOutButton = document.querySelector('[name="opt-out-button"]');
     let runOptOutDtrum = true;
 
+    const getAllCookies = function () {
+        return decodeURIComponent(document.cookie)
+            .split(';')
+            .map(cookie => cookie.trim())
+            .filter(cookie => cookie.length)
+            .map(cookie => {
+                const cookieComponents = cookie.split(/=(.*)/).map(component => component.trim());
+                return { name: cookieComponents[0], value: cookieComponents[1] };
+            });
+    }
+
+    const getCookie = function (name) {
+        return getAllCookies().filter(cookie => cookie.name === name)[0];
+    }
+
     const enterRumAction = function (e) {
         if (runOptOutDtrum) {
             e.preventDefault();
@@ -11,7 +26,10 @@
 
             const dtrum = window.dtrum;
             if (dtrum !== undefined) {
-                if (cookieStatus.apm === 'on') {
+                let cookieStatus = getCookie('pcq-cookie-preferences');
+                cookieStatus = cookieStatus.value ? JSON.parse(cookieStatus.value) : null;
+
+                if (cookieStatus && cookieStatus.apm === 'on') {
                     dtrum.enterAction('PCQ Opt-out')
                 }
             }
