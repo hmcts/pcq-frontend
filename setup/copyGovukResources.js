1db3a850b0214c68ff30232c1047f32c2685f98b
Priyanka Verma <priyanka.verma@hmcts.net>
2024-06-18 13:40:12 +0100

    PCQ-1852 upgarde to 5.0.0

--- setup/copyGovukResources.js
+++ setup/copyGovukResources.js
@@ -5,7 +5,8 @@ const govukFrontend = require.resolve('govuk-frontend');
 const govukDir = path.parse(govukFrontend).dir;
 
 try {
-    copySync(govukDir, 'public/govuk', {overwrite: true});
+    copySync(govukDir + "/govuk-frontend.min.css", 'public/govuk/govuk-frontend.min.css', {overwrite: true});
+    copySync(govukDir + "/govuk-frontend.min.js", 'public/govuk/govuk-frontend.min.js', {overwrite: true});
 } catch (err) {
     console.error(err);
 }
