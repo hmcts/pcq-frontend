c45ef08e21671343f9d839c2103d7761e48fa7b8
Priyanka Verma <priyanka.verma@hmcts.net>
2024-06-18 14:22:28 +0100

    PCQ-1852 upgrade

--- setup/copyGovukResources.js
+++ setup/copyGovukResources.js
@@ -6,6 +6,8 @@ const govukDir = path.parse(govukFrontend).dir;
 
 try {
     copySync(govukDir, 'public/govuk', {overwrite: true});
+    copySync(govukDir + "/govuk-frontend.min.css", 'public/govuk/govuk-frontend.min.css', {overwrite: true});
+    copySync(govukDir + "/govuk-frontend.min.js", 'public/govuk/govuk-frontend.min.js', {overwrite: true});
 } catch (err) {
     console.error(err);
 }
