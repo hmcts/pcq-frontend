4face77ee74e70d82b6500e78ed1198866ec099b
Priyanka Verma <priyanka.verma@hmcts.net>
2024-10-02 16:11:25 +0100

    PCQ-1973 adding optout url

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
             frameAncestors: ['\'self\''],
             formAction: [
                 '\'self\'',
-                '\'/opt-out\''
+                '/opt-out'
             ],
         },
         browserSniff: false,
