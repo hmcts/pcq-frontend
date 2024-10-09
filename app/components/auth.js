ea0700ed5e548033ed89c24066f15db5623ac25c
priyankaVerma21 <priyanka.verma@HMCTS.NET>
2024-10-09 16:23:01 +0100

    PCQ-1983 remove logger info from auth.js

--- app/components/auth.js
+++ app/components/auth.js
@@ -6,7 +6,6 @@ const logger = require('app/components/logger')('Init');
 
 const createToken = (req, partyId, payload = {}) => {
     const conf = config.auth.jwt;
-    logger.info(`Using ${conf.secret === 'JwtSecretKey' ? 'local' : 'Azure KV'} secret`);
 
     payload.authorities = [];
     const token = jwt.sign(payload, conf.secret, {subject: partyId, expiresIn: conf.ttl});
