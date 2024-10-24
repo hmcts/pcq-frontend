5ee6775e5f6979d1addf99ec4a00f499949c1fc5
Priyanka Verma <priyanka.verma@hmcts.net>
2024-10-24 16:43:50 +0100

    PCQ-1986 lint fix

--- app/components/string-utils.js
+++ app/components/string-utils.js
@@ -1,28 +1,28 @@
 'use strict';
 
 const whitelist = [
-    "www.apply-for-probate.service.gov.uk",
-    "www.moneyclaims.service.gov.uk",
-    "www1.moneyclaims.service.gov.uk",
-    "www.apply-divorce.service.gov.uk",
-    "www.respond-divorce.service.gov.uk",
-    "www.appeal-benefit-decision.service.gov.uk",
-    "www.appeal-immigration-asylum-decision.service.gov.uk",
-    "apply-to-adopt-a-child-placed-in-your-care.service.gov.uk",
-    "www.claim-employment-tribunals.service.gov.uk",
-    "onlineplea.cjscp.org.uk",
-    "special-tribunals.service.gov.uk",
-    "reply-jury-summons.service.gov.uk",
-    "www.apply-to-court-about-child-arrangements-c100.service.gov.uk",
-    "www.gov.uk"
-]
+    'www.apply-for-probate.service.gov.uk',
+    'www.moneyclaims.service.gov.uk',
+    'www1.moneyclaims.service.gov.uk',
+    'www.apply-divorce.service.gov.uk',
+    'www.respond-divorce.service.gov.uk',
+    'www.appeal-benefit-decision.service.gov.uk',
+    'www.appeal-immigration-asylum-decision.service.gov.uk',
+    'apply-to-adopt-a-child-placed-in-your-care.service.gov.uk',
+    'www.claim-employment-tribunals.service.gov.uk',
+    'onlineplea.cjscp.org.uk',
+    'special-tribunals.service.gov.uk',
+    'reply-jury-summons.service.gov.uk',
+    'www.apply-to-court-about-child-arrangements-c100.service.gov.uk',
+    'www.gov.uk'
+];
 
 const prefixHttps = (url = '') => {
     url = url.trim().replace(/\s/g, '');
     if (!(/^(?:f|ht)tps?:\/\//).test(url)) {
         url = 'https://' + url;
         if(!(isUrlWhitelisted(url) || url.includes('platform.hmcts.net') || url.endsWith('test.gov.uk'))) {
-            url =''
+            url ='';
         }
     }
     return url;
