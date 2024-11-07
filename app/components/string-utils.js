3d383c32112807b0a0571fe92d8f3f21ef08fc1e
Priyanka Verma <priyanka.verma@hmcts.net>
2024-11-07 11:08:21 +0000

    PCQ-1986 just whitelisting gov.uk

--- app/components/string-utils.js
+++ app/components/string-utils.js
@@ -1,29 +1,12 @@
 'use strict';
 
-const whitelist = [
-    'www.apply-for-probate.service.gov.uk',
-    'www.moneyclaims.service.gov.uk',
-    'www1.moneyclaims.service.gov.uk',
-    'www.ocmc.service.gov.uk',
-    'www.apply-divorce.service.gov.uk',
-    'www.respond-divorce.service.gov.uk',
-    'www.end-civil-partnership.service.gov.uk',
-    'www.appeal-benefit-decision.service.gov.uk',
-    'www.appeal-immigration-asylum-decision.service.gov.uk',
-    'apply-to-adopt-a-child-placed-in-your-care.service.gov.uk',
-    'www.claim-employment-tribunals.service.gov.uk',
-    'onlineplea.cjscp.org.uk',
-    'special-tribunals.service.gov.uk',
-    'reply-jury-summons.service.gov.uk',
-    'www.apply-to-court-about-child-arrangements-c100.service.gov.uk'
-];
-
 const prefixHttps = (url = '') => {
     url = url.trim().replace(/\s/g, '');
     if (!(/^(?:f|ht)tps?:\/\//).test(url)) {
         url = 'https://' + url;
     }
-    if(!(isUrlWhitelisted(url) || url.includes('platform.hmcts.net') 
+    if(!(url.includes('service.gov.uk') || url.includes('cjscp.org.uk')
+        || url.includes('platform.hmcts.net') 
         || url.includes('staging.apps.hmcts.net')
         || url.endsWith('test.gov.uk') 
         || url.startsWith('https://localhost') || url.startsWith('http://localhost'))){
@@ -31,12 +14,6 @@ const prefixHttps = (url = '') => {
     }
     return url;
 };
-
-function isUrlWhitelisted(url) {
-    // Extract the hostname from the URL
-    const hostname = new URL(url).hostname;
-    return whitelist.includes(hostname);
-}
 
 module.exports = {
     prefixHttps
