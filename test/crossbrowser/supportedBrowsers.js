ebbdef42bd1c59a19d403602dd30aad32831b859
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-10-22 19:56:29 +0100

    update supported browsers

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -2,10 +2,10 @@ const supportedBrowsers = {
     microsoftEdge: {
         edge: {
             browserName: 'MicrosoftEdge',
-            platformName: 'Windows 10',
-            browserVersion: '18.17763',
+            platformName: 'Windows 11',
+            browserVersion: 'latest',
             'sauce:options': {
-                name: 'PCQ_WIN10_EDGE_18',
+                name: 'PCQ_WIN11_EDGE_LATEST',
             }
         }
     },
@@ -13,16 +13,16 @@ const supportedBrowsers = {
         chrome_win_latest: {
             browserName: 'chrome',
             browserVersion: 'latest',
-            platformName: 'Windows 10',
+            platformName: 'Windows 11',
             'sauce:options': {
-                name: 'PCQ_WIN10_CHROME_LATEST',
+                name: 'PCQ_WIN11_CHROME_LATEST',
                 extendedDebugging: true
             }
         },
         chrome_mac_latest: {
             browserName: 'chrome',
             browserVersion: 'latest',
-            platformName: 'macOS 10.13',
+            platformName: 'macOS 15',
             'sauce:options': {
                 name: 'PCQ_MAC_CHROME_LATEST',
                 extendedDebugging: true
@@ -32,16 +32,16 @@ const supportedBrowsers = {
     firefox: {
         firefox_win_latest: {
             browserName: 'firefox',
-            platformName: 'Windows 10',
+            platformName: 'Windows 11',
             browserVersion: 'latest',
             'sauce:options': {
-                name: 'PCQ_WIN10_FIREFOX_LATEST',
+                name: 'PCQ_WIN11_FIREFOX_LATEST',
                 extendedDebugging: true
             }
         },
         firefox_mac_latest: {
             browserName: 'firefox',
-            platformName: 'macOS 10.13',
+            platformName: 'macOS 13',
             browserVersion: 'latest',
             'sauce:options': {
                 name: 'PCQ_MAC_FIREFOX_LATEST',
@@ -50,12 +50,21 @@ const supportedBrowsers = {
         },
     },
     safari: {
-        safari11: {
+        safari_latest_mac_arm: {
             browserName: 'safari',
-            browserVersion: '11.1',
-            platformName: 'macOS 10.13',
+            browserVersion: 'latest',
+            platformName: 'macOS 15',
             'sauce:options': {
-                name: 'PCQ_MAC_SAFARI_11',
+                armRequired: true,
+                name: 'PCQ_MAC_SAFARI_LATEST_ARM',
+            }
+        },
+        safari_latest_mac_intel: {
+            browserName: 'safari',
+            browserVersion: 'latest',
+            platformName: 'macOS 15',
+            'sauce:options': {
+                name: 'PCQ_MAC_SAFARI_LATEST_INTEL',
             }
         }
     }
