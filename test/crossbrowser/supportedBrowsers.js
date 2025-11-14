7ec87303a6a85c4f1c2afe2b49a77f995e8b414f
Priyanka Verma <priyanka.verma@hmcts.net>
2025-11-14 16:35:08 +0000

    Remove ios and other

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -11,7 +11,7 @@ const supportedBrowsers = {
   },
 
   chrome: {
-        /*chrome_win_latest: {
+        chrome_win_latest: {
             browserName: 'chrome',
             browserVersion: 'latest',
             platformName: 'Windows 11',
@@ -19,19 +19,7 @@ const supportedBrowsers = {
                 name: 'PCQ_WIN11_CHROME_LATEST',
                 extendedDebugging: true
             }
-        },*/
-        chrome_android_16: {
-            platformName: 'Android',
-            browserName: 'Chrome',
-            'appium:deviceName': 'Google Pixel 9 Pro Emulator',
-            'appium:platformVersion': '16.0',
-            'appium:automationName': 'UiAutomator2',
-            'sauce:options': {
-                appiumVersion: '2.11.0',
-                name: 'PCQ_ANDROID_CHROME_16',
-                deviceOrientation: 'PORTRAIT',
-            }
-        },
+          }
         
   },
 
@@ -52,19 +40,6 @@ const supportedBrowsers = {
             'sauce:options': {
                 name: 'PCQ_MAC_FIREFOX_LATEST',
                 extendedDebugging: true
-            }
-        }
-  },
-  safari: {
-        safari_ios_simulator_17: {
-            browserName: 'Safari',
-            platformName: 'iOS',
-            'appium:deviceName': 'iPhone Simulator',
-            'appium:platformVersion': '17.0',
-            'appium:automationName': 'XCUITest',
-            'sauce:options': {
-                appiumVersion: '2.1.3',
-                name: 'PCQ_IOS_SAFARI_17',
             }
         }
   }
