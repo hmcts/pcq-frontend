5d48d7a322cf2832a34dbedc5c7e30e79f16b9e6
Priyanka Verma <priyanka.verma@hmcts.net>
2025-11-14 14:20:18 +0000

    PCQ-2011 upgrade codeceptjs to 3.7.5

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -19,15 +19,6 @@ const supportedBrowsers = {
                 name: 'PCQ_WIN11_CHROME_LATEST',
                 extendedDebugging: true
             }
-        },
-        chrome_mac_latest: {
-            browserName: 'chrome',
-            browserVersion: 'latest',
-            platformName: 'macOS 11',
-            'sauce:options': {
-                name: 'PCQ_MAC_CHROME_LATEST',
-                extendedDebugging: true
-            }
         }
   },
 
@@ -50,17 +41,7 @@ const supportedBrowsers = {
                 extendedDebugging: true
             }
         }
-  },
-  safari: {
-        safari_mac_latest: {
-            browserName: 'safari',
-            browserVersion: 'latest',
-            platformName: 'macOS 11',
-            'sauce:options': {
-                name: 'PCQ_MAC_SAFARI_LATEST',
-            }
-        }
-  }
+    }
   
 };
 
