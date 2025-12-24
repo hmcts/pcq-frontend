ba253fd3d6822a6e28da68152f81e939ffc93c79
Priyanka Verma <priyanka.verma@hmcts.net>
2025-12-24 17:08:09 +0000

    upgrade to 8.0.0

--- .pnp.cjs
+++ .pnp.cjs
@@ -64,7 +64,7 @@ const RAW_RUNTIME_STATE =
           ["git-rev-sync", "npm:3.0.2"],\
           ["globals", "npm:16.5.0"],\
           ["govuk-frontend", "npm:5.13.0"],\
-          ["helmet", "npm:6.2.0"],\
+          ["helmet", "npm:8.1.0"],\
           ["https-proxy-agent", "npm:5.0.1"],\
           ["i18next", "virtual:4988fc10d27710cdb3e57dc0e556fd1d3b78e0a4d35d5725b1ddaa965a40cf5f6ea242a6d7b282a5264da04a4e9f3a153d26f33d87257cd2a848cb2105ced64c#npm:25.7.3"],\
           ["immutable", "npm:5.1.4"],\
@@ -12201,10 +12201,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["helmet", [\
-      ["npm:6.2.0", {\
-        "packageLocation": "./.yarn/cache/helmet-npm-6.2.0-43622c54ea-f112fcd0d8.zip/node_modules/helmet/",\
+      ["npm:8.1.0", {\
+        "packageLocation": "./.yarn/cache/helmet-npm-8.1.0-a2193b660c-262e678d34.zip/node_modules/helmet/",\
         "packageDependencies": [\
-          ["helmet", "npm:6.2.0"]\
+          ["helmet", "npm:8.1.0"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -17255,7 +17255,7 @@ const RAW_RUNTIME_STATE =
           ["git-rev-sync", "npm:3.0.2"],\
           ["globals", "npm:16.5.0"],\
           ["govuk-frontend", "npm:5.13.0"],\
-          ["helmet", "npm:6.2.0"],\
+          ["helmet", "npm:8.1.0"],\
           ["https-proxy-agent", "npm:5.0.1"],\
           ["i18next", "virtual:4988fc10d27710cdb3e57dc0e556fd1d3b78e0a4d35d5725b1ddaa965a40cf5f6ea242a6d7b282a5264da04a4e9f3a153d26f33d87257cd2a848cb2105ced64c#npm:25.7.3"],\
           ["immutable", "npm:5.1.4"],\
