4a63aaef77a18af2385acd8ad00dc7720fa92f5c
priyankaVerma21 <priyanka.verma@HMCTS.NET>
2026-01-12 16:01:03 +0000

    Update testUtils.js

--- test/unit/components/testUtils.js
+++ test/unit/components/testUtils.js
@@ -3,8 +3,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const session = require('express-session');
-const Module = require('module');
-
 const utils = require('app/components/utils');
 
 describe('api-utils', () => {
@@ -59,71 +57,26 @@ describe('api-utils', () => {
     });
 
     describe('getStore', () => {
-        let originalIoredis;
-        let ioredisPath;
-
-        const createFakeRedisModule = () => {
-            function FakeRedis() {
-                this._handlers = {};
-            }
-            FakeRedis.prototype.on = function(event, handler) {
-                this._handlers[event] = handler;
-                if (event === 'ready') {
-                    process.nextTick(handler);
-                }
-            };
-            FakeRedis.prototype.ping = function() {};
-            FakeRedis.prototype.end = function() {
-                if (this._handlers.end) {
-                    this._handlers.end();
-                }
-            };
-            FakeRedis.prototype.quit = FakeRedis.prototype.end;
-            FakeRedis.prototype.disconnect = FakeRedis.prototype.end;
-
-            const fakeModule = new Module(ioredisPath);
-            fakeModule.exports = FakeRedis;
-            fakeModule.loaded = true;
-            return fakeModule;
+        it('creates a valid RedisStore', () => {
+        const redisConfig = {
+            enabled: 'true',
+            password: 'secure',
+            useTLS: 'false',   //  avoid TLS in unit tests
+            host: 'localhost',
+            port: '6379',
+            keepAlive: 'false' // disable ping interval
         };
 
-        beforeEach(() => {
-            ioredisPath = require.resolve('ioredis');
-            originalIoredis = require.cache[ioredisPath];
-            require.cache[ioredisPath] = createFakeRedisModule();
-        });
+        const redisStore = utils.getStore(redisConfig, session);
+        const redisStoreName = redisStore.constructor.name;
 
-        afterEach(() => {
-            if (originalIoredis) {
-                require.cache[ioredisPath] = originalIoredis;
-            } else {
-                delete require.cache[ioredisPath];
-            }
-        });
+        //ioredis uses disconnect(), not end()
+        if (redisStore?.client) {
+            redisStore.client.disconnect();
+        }
 
-        it('creates a valid RedisStore', () => {
-            const redisConfig = {
-                enabled: 'true',
-                password: 'secure',
-                useTLS: 'true',
-                host: 'localhost',
-                port: '6379',
-                keepAlive: 'false'
-            };
-            const redisStore = utils.getStore(redisConfig, session);
-            const redisStoreName = redisStore.constructor.name;
-
-            // End and destroy before expect in case of error. These will hang the tests if not run.
-            if (redisStore.client) {
-                if (typeof redisStore.client.end === 'function') {
-                    redisStore.client.end();
-                } else if (typeof redisStore.client.quit === 'function') {
-                    redisStore.client.quit();
-                } else if (typeof redisStore.client.disconnect === 'function') {
-                    redisStore.client.disconnect();
-                }
-            }
-            redisStore.destroy();
+        // Destroy the store after client disconnect
+        redisStore.destroy();
 
             expect(redisStoreName).to.equal('RedisStore');
         });
