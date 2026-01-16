1851574c5cd553e8e7ec480003d7d919b3f4b461
Priyanka Verma <priyanka.verma@hmcts.net>
2026-01-16 14:34:33 +0000

    remove baseuri

--- app.js
+++ app.js
@@ -95,8 +95,6 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
         directives: {
             defaultSrc: ['\'self\''],
             fontSrc: ['\'self\'', 'data:', 'fonts.gstatic.com'],   
-            baseUri: ['\'self\''],        
-            objectSrc: ['\'none\''],
             scriptSrc: [
                 '\'self\'',
                 '\'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=\'',
