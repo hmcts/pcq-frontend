a92916322e1403acfdd78e9d1c8ce2c5a615d5ed
Priyanka Verma <priyanka.verma@hmcts.net>
2026-01-22 12:09:38 +0000

    PCQ-2087-adding issuer and audience in JWTToken

--- app/components/auth.js
+++ app/components/auth.js
@@ -7,7 +7,12 @@ const createToken = (req, partyId, payload = {}) => {
     const conf = config.auth.jwt;
 
     payload.authorities = [];
-    const token = jwt.sign(payload, conf.secret, {subject: partyId, expiresIn: conf.ttl});
+    const token = jwt.sign(payload, conf.secret, {
+        subject: partyId,
+        expiresIn: conf.ttl,
+        issuer: conf.issuer,
+        audience: conf.audience
+    });
 
     req.session.token = token;
     return token;
