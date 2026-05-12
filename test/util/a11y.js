f31d997ee34c3713fa6568ae8d7c95dc8af6cb45
shabmama53 <shabbir.mama@justice.gov.uk>
2026-05-12 11:26:49 +0100

    adding playwright install chromium  (#1891)

--- test/util/a11y.js
+++ test/util/a11y.js
@@ -6,6 +6,10 @@ module.exports = (testPage) => {
     return new Promise((resolve, reject) => {
         pa11y(testPage, {
             includeWarnings: true,
+            chromeLaunchConfig: {
+                executablePath: process.env.CHROME_PATH || '/usr/bin/google-chrome',
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            },
             hideElements: '.govuk-cookie-banner__heading, .govuk-box-highlight, .govuk-header__logo, .govuk-footer, link[rel=mask-icon], .govuk-skip-link, .govuk-button--start, .govuk-visually-hidden, .govuk-warning-text__assistive, iframe, #ctsc-web-chat, .govuk-warning-text__icon',
         }, (err, results) => {
             if (err) {
