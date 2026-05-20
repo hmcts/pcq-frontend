cf09c96b77c587be8a7f440127bc30acd9f8685f
Priyanka Verma <priyanka.verma@hmcts.net>
2026-05-20 18:08:06 +0100

    fix service journey

--- test/unit/core/testServiceJourney.js
+++ test/unit/core/testServiceJourney.js
@@ -4,7 +4,11 @@ const expect = require('chai').expect;
 const Proxyquire = require('proxyquire/lib/proxyquire');
 const proxyquire = new Proxyquire(module).noCallThru();
 const initSteps = require('app/core/initSteps');
-const steps = initSteps([`${__dirname}/../../../app/steps/ui`]);
+const steps = initSteps([
+    `${__dirname}/../../../app/steps/ui/startpage`,
+    `${__dirname}/../../../app/steps/ui/endpage`,
+    `${__dirname}/../../../app/steps/ui/shutterpage`
+]);
 const serviceData = require('test/unit/core/testServiceData.json');
 const StartPage = steps.StartPage;
 const EndPage = steps.EndPage;
@@ -21,100 +25,92 @@ const buildJourneyMap = (steps) => {
 //joining path of directory
 const directoryPath = path.join(`${__dirname}/../../../app`, 'journeys');
 
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // your tests logic
-        console.log(file);
-        const filePathFragments = file.split('.');
-        const serviceName = filePathFragments[0];
-        if (filePathFragments[1] === 'js') {
-            describe('ServiceJourney : ' + serviceName, () => {
-                const currentStep = {};
-                const serviceJourney = require(`app/journeys/${serviceName}`)();
-                const skipStepName = serviceData.services[serviceName].skipStepName;
-                describe('stepList()', () => {
-                    it('should return the journey step list without skip list', (done) => {
-                        const JourneyMap = buildJourneyMap(steps);
-                        const journeyMap = new JourneyMap(serviceJourney);
-                        const stepList = journeyMap.stepList();
-                        if (stepList !== null) {
-                            expect(stepList).to.not.contain(skipStepName);
-                        }
-                        done();
-                    });
+const files = fs.readdirSync(directoryPath);
+files.forEach(function (file) {
+    console.log(file);
+    const filePathFragments = file.split('.');
+    const serviceName = filePathFragments[0];
+    if (filePathFragments[1] === 'js') {
+        describe('ServiceJourney : ' + serviceName, () => {
+            const currentStep = {};
+            const serviceJourney = require(`app/journeys/${serviceName}`)();
+            const skipStepName = serviceData.services[serviceName].skipStepName;
+            describe('stepList()', () => {
+                it('should return the journey step list without skip list', (done) => {
+                    const JourneyMap = buildJourneyMap(steps);
+                    const journeyMap = new JourneyMap(serviceJourney);
+                    const stepList = journeyMap.stepList();
+                    if (stepList !== null) {
+                        expect(stepList).to.not.contain(skipStepName);
+                    }
+                    done();
                 });
-                describe('nextStep()', () => {
-                    let journey;
-                    let JourneyMap;
-                    const nextStep = serviceData.services[serviceName].nextStep;
-                    const nextStepName = serviceData.services[serviceName].nextStepName;
-                    beforeEach(() => {
-                        JourneyMap = buildJourneyMap(nextStep);
-                        journey = serviceJourney;
-                    });
-                    it('should skip a step and go to next step as mentioned in service', (done) => {
-                        currentStep.name = serviceData.services[serviceName].currentStep;
-                        const ctx = {};
-                        const journeyMap = new JourneyMap(journey);
-                        const nextStep = journeyMap.nextStep(currentStep, ctx);
-                        expect(nextStep).to.deep.equal(nextStepName);
-                        done();
-                    });
-                });
-
-                const datas = serviceData.services[serviceName].datas;
-                for (const data in datas) {
-                    describe('Actor : '+ data, () => {
-                        describe('generateStartPageContent()', () => {
-                            const formData = serviceData.services[serviceName].datas[data].formData;
-                            const startPageTextEn = serviceData.services[serviceName].datas[data].startPageTextEn;
-                            const startPageTextCy = serviceData.services[serviceName].datas[data].startPageTextCy;
-                            it('should return variable text for a service', () => {
-                                const content = StartPage.generateContent({}, formData);
-                                expect(content.paragraph2).to.equal(startPageTextEn);
-                            });
-                            it('should return variable text for a service in welsh', () => {
-                                const content = StartPage.generateContent({}, formData, 'cy');
-                                expect(content.paragraph2).to.equal(startPageTextCy);
-                            });
-                        });
-
-                        describe('generateEndPageContent()', () => {
-                            const formData = serviceData.services[serviceName].datas[data].formData;
-                            const endPageTextEn = serviceData.services[serviceName].datas[data].endPageTextEn;
-                            const endPageTextCy = serviceData.services[serviceName].datas[data].endPageTextCy;
-                            it('should return variable text for a service', () => {
-                                const content = EndPage.generateContent({}, formData);
-                                expect(content.paragraph1).to.equal(endPageTextEn);
-                            });
-                            it('should return variable text for a service in welsh', () => {
-                                const content = EndPage.generateContent({}, formData, 'cy');
-                                expect(content.paragraph1).to.equal(endPageTextCy);
-                            });
-                        });
-
-                        describe('generateShutterPageContent()', () => {
-                            const formData = serviceData.services[serviceName].datas[data].formData;
-                            const shutterPageTextEn = serviceData.services[serviceName].datas[data].shutterPageTextEn;
-                            const shutterPageTextCy = serviceData.services[serviceName].datas[data].shutterPageTextCy;
-                            it('should return variable text for a service', () => {
-                                const content = ShutterPage.generateContent({}, formData);
-                                expect(content.paragraph1).to.equal(shutterPageTextEn);
-                            });
-                            it('should return variable text for a service in welsh', () => {
-                                const content = ShutterPage.generateContent({}, formData, 'cy');
-                                expect(content.paragraph1).to.equal(shutterPageTextCy);
-                            });
-                        });
-                    });
-                }
             });
-        }
-    });
+            describe('nextStep()', () => {
+                let journey;
+                let JourneyMap;
+                const nextStep = serviceData.services[serviceName].nextStep;
+                const nextStepName = serviceData.services[serviceName].nextStepName;
+                beforeEach(() => {
+                    JourneyMap = buildJourneyMap(nextStep);
+                    journey = serviceJourney;
+                });
+                it('should skip a step and go to next step as mentioned in service', (done) => {
+                    currentStep.name = serviceData.services[serviceName].currentStep;
+                    const ctx = {};
+                    const journeyMap = new JourneyMap(journey);
+                    const nextStep = journeyMap.nextStep(currentStep, ctx);
+                    expect(nextStep).to.deep.equal(nextStepName);
+                    done();
+                });
+            });
+
+            const datas = serviceData.services[serviceName].datas;
+            for (const data in datas) {
+                describe('Actor : ' + data, () => {
+                    describe('generateStartPageContent()', () => {
+                        const formData = serviceData.services[serviceName].datas[data].formData;
+                        const startPageTextEn = serviceData.services[serviceName].datas[data].startPageTextEn;
+                        const startPageTextCy = serviceData.services[serviceName].datas[data].startPageTextCy;
+                        it('should return variable text for a service', () => {
+                            const content = StartPage.generateContent({}, formData);
+                            expect(content.paragraph2).to.equal(startPageTextEn);
+                        });
+                        it('should return variable text for a service in welsh', () => {
+                            const content = StartPage.generateContent({}, formData, 'cy');
+                            expect(content.paragraph2).to.equal(startPageTextCy);
+                        });
+                    });
+
+                    describe('generateEndPageContent()', () => {
+                        const formData = serviceData.services[serviceName].datas[data].formData;
+                        const endPageTextEn = serviceData.services[serviceName].datas[data].endPageTextEn;
+                        const endPageTextCy = serviceData.services[serviceName].datas[data].endPageTextCy;
+                        it('should return variable text for a service', () => {
+                            const content = EndPage.generateContent({}, formData);
+                            expect(content.paragraph1).to.equal(endPageTextEn);
+                        });
+                        it('should return variable text for a service in welsh', () => {
+                            const content = EndPage.generateContent({}, formData, 'cy');
+                            expect(content.paragraph1).to.equal(endPageTextCy);
+                        });
+                    });
+
+                    describe('generateShutterPageContent()', () => {
+                        const formData = serviceData.services[serviceName].datas[data].formData;
+                        const shutterPageTextEn = serviceData.services[serviceName].datas[data].shutterPageTextEn;
+                        const shutterPageTextCy = serviceData.services[serviceName].datas[data].shutterPageTextCy;
+                        it('should return variable text for a service', () => {
+                            const content = ShutterPage.generateContent({}, formData);
+                            expect(content.paragraph1).to.equal(shutterPageTextEn);
+                        });
+                        it('should return variable text for a service in welsh', () => {
+                            const content = ShutterPage.generateContent({}, formData, 'cy');
+                            expect(content.paragraph1).to.equal(shutterPageTextCy);
+                        });
+                    });
+                });
+            }
+        });
+    }
 });
