2fb9dc3ae9d5bd5e880cfb86674b7e5b1087fe3d
Priyanka Verma <priyanka.verma@hmcts.net>
2026-05-20 15:28:49 +0100

    use proxyquire instead of rewire

--- test/unit/components/testVerifyToken.js
+++ test/unit/components/testVerifyToken.js
@@ -2,20 +2,38 @@
 
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const rewire = require('rewire');
-const {verifyToken} = require('app/components/encryption-token');
+const proxyquire = require('proxyquire').noCallThru();
+const config = require('config');
+const tokenModule = require('app/components/encryption-token');
+
+const buildParams = (serviceId = 'PROBATE', returnUrl = 'test.com') => ({
+    serviceId,
+    actor: 'APPLICANT',
+    pcqId: '12345',
+    partyId: 'test@test.com',
+    returnUrl
+});
+
+const buildTokenModuleWithKey = (tokenKey = 'REGISTERED_TOKEN_KEY') => {
+    const loggerStub = {info: sinon.spy(), error: sinon.spy(), warn: sinon.spy()};
+    return proxyquire('app/components/encryption-token', {
+        config: {
+            ...config,
+            tokenKeys: {
+                ...config.tokenKeys,
+                registered: tokenKey
+            },
+            get: config.get
+        },
+        'app/components/logger': () => loggerStub
+    });
+};
 
 describe('VerifyToken', () => {
     it('should fail verification if token is missing from req query', (done) => {
-        const reqQuery = {
-            serviceId: 'PROBATE',
-            actor: 'APPLICANT',
-            pcqId: '12345',
-            partyId: 'test@test.com',
-            returnUrl: 'test.com'
-        };
+        const reqQuery = buildParams();
 
-        expect(verifyToken(reqQuery)).to.equal(false);
+        expect(tokenModule.verifyToken(reqQuery)).to.equal(false);
         done();
     });
 
@@ -28,7 +46,7 @@ describe('VerifyToken', () => {
             token: 'token'
         };
 
-        expect(verifyToken(reqQuery)).to.equal(false);
+        expect(tokenModule.verifyToken(reqQuery)).to.equal(false);
         done();
     });
 
@@ -42,7 +60,7 @@ describe('VerifyToken', () => {
             token: 'token'
         };
 
-        expect(verifyToken(reqQuery)).to.equal(false);
+        expect(tokenModule.verifyToken(reqQuery)).to.equal(false);
         done();
     });
 
@@ -56,128 +74,91 @@ describe('VerifyToken', () => {
             token: 'mismatched_token'
         };
 
-        expect(verifyToken(reqQuery)).to.equal(false);
+        expect(tokenModule.verifyToken(reqQuery)).to.equal(false);
         done();
     });
 
     it('should successfully verify a valid req query with a matching token', (done) => {
-        const verifyTokenRewired = rewire('app/components/encryption-token');
-        verifyTokenRewired.__set__('config', {
-            tokenKeys: {
-                registered: 'REGISTERED_TOKEN_KEY'
-            }
-        });
-
+        const params = buildParams('REGISTERED');
+        const moduleWithKnownKey = buildTokenModuleWithKey();
         const reqQuery = {
-            serviceId: 'REGISTERED',
-            actor: 'APPLICANT',
-            pcqId: '12345',
-            partyId: 'test@test.com',
-            returnUrl: 'test.com',
+            ...params,
             token: '4ab384df14b77f245d02b33f2f5176ec08d372ab18100688f9c62560b2f5aa17ffc1870b7eef5d4b67413eeff1999f2' +
                 'fb5953843b3b284329cbfd54cd7713fdf94f5cad773fd5f751bc28957898152917b15b81b70e217e531174a256a86a' +
                 'ae5c04287db6139fdb172ce78ac3af8d8'
         };
 
-        expect(verifyTokenRewired.verifyToken(reqQuery)).to.equal(true);
+        expect(moduleWithKnownKey.verifyToken(reqQuery)).to.equal(true);
         done();
     });
 
     it('should successfully verify a valid req query with a matching token using legacy encryption', (done) => {
-        const verifyTokenRewired = rewire('app/components/encryption-token');
-        verifyTokenRewired.__set__('config', {
-            tokenKeys: {
-                registered: 'REGISTERED_TOKEN_KEY'
-            }
-        });
-
+        const params = buildParams('REGISTERED', 'REGISTERED_APPLICANT.test.gov.uk');
+        const moduleWithKnownKey = buildTokenModuleWithKey();
         const reqQuery = {
-            serviceId: 'REGISTERED',
-            actor: 'APPLICANT',
-            pcqId: '12345',
-            partyId: 'test@test.com',
-            returnUrl: 'REGISTERED_APPLICANT.test.gov.uk',
+            ...params,
             token: '05ae10bf33f9b7dbc1f5e5e3636565ed974cf7e1ed14315dbd2890d6884a70d0a51c9042769c'+
                 'f1804052fb367d5a8db7d1214e43fad2d70eba7bf9ccb3780db76471bdfcc20c3840efde9f4d325'+
                 'a5f6fb02075357b7e7bafae2517426a126d009cd94e49c85aae58ec6e438b4d9632cf72045d82ca12'+
                 'c1e1fadb522fd3fab85e7546af19962364d34105a1329c9a60ee'
         };
 
-        expect(verifyTokenRewired.verifyToken(reqQuery)).to.equal(true);
+        expect(moduleWithKnownKey.verifyToken(reqQuery)).to.equal(true);
         done();
     });
 
     it('should log auth tag when logging is enabled', () => {
-        const tokenModule = rewire('app/components/encryption-token');
-        const loggerStub = {info: sinon.spy(), error: sinon.spy()};
-        tokenModule.__set__('logger', loggerStub);
-        tokenModule.__set__('config', {
+        const loggerStub = {info: sinon.spy(), error: sinon.spy(), warn: sinon.spy()};
+        const configStub = {
+            ...config,
             tokenKeys: {
+                ...config.tokenKeys,
                 registered: 'REGISTERED_TOKEN_KEY'
-            }
-        });
-        tokenModule.__set__('shouldLog', true);
+            },
+            get: sinon.stub(config, 'get').callsFake((key) => {
+                if (key === 'loggingEnabled') {
+                    return 'true';
+                }
+                return config[key];
+            })
+        };
 
-        const result = tokenModule.generateToken({
-            serviceId: 'REGISTERED',
-            actor: 'APPLICANT',
-            pcqId: '12345',
-            partyId: 'test@test.com',
-            returnUrl: 'test.com'
+        const tokenModuleWithLogging = proxyquire('app/components/encryption-token', {
+            config: configStub,
+            'app/components/logger': () => loggerStub
         });
+
+        const result = tokenModuleWithLogging.generateToken(buildParams('REGISTERED'));
+        configStub.get.restore();
 
         expect(result.authTag).to.not.equal('');
         sinon.assert.calledWith(loggerStub.info, sinon.match(/^Auth Tag : /));
     });
 
     it('should successfully verify a secure token using authTag, iv and salt', () => {
-        const tokenModule = rewire('app/components/encryption-token');
-        tokenModule.__set__('config', {
-            tokenKeys: {
-                registered: 'REGISTERED_TOKEN_KEY'
-            }
-        });
+        const moduleWithKnownKey = buildTokenModuleWithKey();
+        const params = buildParams('REGISTERED');
 
-        const params = {
-            serviceId: 'REGISTERED',
-            actor: 'APPLICANT',
-            pcqId: '12345',
-            partyId: 'test@test.com',
-            returnUrl: 'test.com'
-        };
-
-        const secureToken = tokenModule.generateSecureToken(params);
+        const secureToken = moduleWithKnownKey.generateSecureToken(params);
         const reqQuery = {
             ...params,
             ...secureToken
         };
 
-        expect(tokenModule.verifyToken(reqQuery)).to.equal(true);
+        expect(moduleWithKnownKey.verifyToken(reqQuery)).to.equal(true);
     });
 
     it('should fail verification when secure token auth tag is tampered', () => {
-        const tokenModule = rewire('app/components/encryption-token');
-        tokenModule.__set__('config', {
-            tokenKeys: {
-                registered: 'REGISTERED_TOKEN_KEY'
-            }
-        });
+        const moduleWithKnownKey = buildTokenModuleWithKey();
+        const params = buildParams('REGISTERED');
 
-        const params = {
-            serviceId: 'REGISTERED',
-            actor: 'APPLICANT',
-            pcqId: '12345',
-            partyId: 'test@test.com',
-            returnUrl: 'test.com'
-        };
-
-        const secureToken = tokenModule.generateSecureToken(params);
+        const secureToken = moduleWithKnownKey.generateSecureToken(params);
         const reqQuery = {
             ...params,
             ...secureToken,
             authTag: secureToken.authTag.slice(0, -2) + 'AA'
         };
 
-        expect(tokenModule.verifyToken(reqQuery)).to.equal(false);
+        expect(moduleWithKnownKey.verifyToken(reqQuery)).to.equal(false);
     });
 });
