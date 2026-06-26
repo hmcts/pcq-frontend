b292cb949c14fd2e18814d27fe589cd8eb0241a5
Priyanka Verma <priyanka.verma@hmcts.net>
2026-06-26 15:45:35 +0100

    remove saucelab test

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -ex
 
-# Setup required for Saucelabs environment variables. TEST_URL should be set by CNP
+# Setup required for cross-browser tests. TEST_URL should be set by CNP
 export E2E_FRONTEND_URL=${TEST_URL}
 export IGNORE_SESSION_VALIDATION=true
 
-EXIT_STATUS=0
-BROWSER_GROUP=microsoftEdge yarn test-crossbrowser-e2e || EXIT_STATUS=$?
-BROWSER_GROUP=chrome yarn test-crossbrowser-e2e || EXIT_STATUS=$?
-BROWSER_GROUP=firefox yarn test-crossbrowser-e2e || EXIT_STATUS=$?
-
-echo EXIT_STATUS: $EXIT_STATUS
-exit $EXIT_STATUS
+yarn test:crossbrowser
