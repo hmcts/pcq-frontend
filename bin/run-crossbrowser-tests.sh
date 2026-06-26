0373370a7c240f429a321d4061008eab87ec7e33
Priyanka Verma <priyanka.verma@hmcts.net>
2026-06-26 15:48:22 +0100

    remove crossbrowsertest.sh

--- bin/run-crossbrowser-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Setup required for cross-browser tests. TEST_URL should be set by CNP
-export E2E_FRONTEND_URL=${TEST_URL}
-export IGNORE_SESSION_VALIDATION=true
-
-yarn test:crossbrowser
