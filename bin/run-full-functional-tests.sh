bca2dcb0966b1b733b633c4f95f3deb256ccbfbe
Priyanka Verma <priyanka.verma@hmcts.net>
2026-06-29 15:51:07 +0100

    full functional test

--- bin/run-full-functional-tests.sh
+++ bin/run-full-functional-tests.sh
@@ -2,3 +2,4 @@
 set -ex
 
 NODE_ENV=testing ALLOW_CONFIG_MUTATIONS=true yarn codeceptjs run -c ./test/end-to-end/codecept.conf.js
+NODE_ENV=testing NODE_PATH=. playwright test -c test/playwright/playwright.config.js
